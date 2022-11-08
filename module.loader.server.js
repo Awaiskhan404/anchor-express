36bb1c34ab38a34aa5ea58c5dfde93c31926b92b
Awais khan <contact@awaiskhan.com.pk>
2022-11-08 16:34:38 +0500

    exceptions fix

--- module.loader.server.js
+++ module.loader.server.js
@@ -15,8 +15,13 @@ class ModuleLoader {
             if (dirs.length > 0 && dirs !== undefined) {
                 dirs.forEach((dir) => {
                     console.log(`Module ${dir} loading...`)
-                    const module = require(`${process.cwd()}/${section_}/${dir}/router`);
-                    this.__modules__.push(module);
+                    try {
+                        const module = require(`${process.cwd()}/${section_}/${dir}/router`);
+                        this.__modules__.push(module);
+                    }
+                    catch (e) {
+                        console.log(`Aborted module ${dir} routes not found!`)
+                    }
                 });
             }
             return this.__modules__;
